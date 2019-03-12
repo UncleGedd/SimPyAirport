3842376fc1a4943b928bc2829a3dd1c8f1714102
Gedd <gedd@Gedds-MacBook-Pro.local>
2019-03-11 21:18:09 -0400

    Fixes .gitignore

---
+++ IncreasingServers.py
@@ -0,0 +1,123 @@
+import random
+import simpy
+import numpy as np
+import matplotlib.pyplot as plt
+
+GRAND_TOTAL_SERVER_TIME = []
+GRAND_TOTAL_CHECKER_TIME = []
+GRAND_TOTAL_WAIT_TIME = []
+GRAND_TOTAL_SYSTEM_TIME = []
+for numServers in range(1, 5):
+    random.seed(7)
+    ARRIVAL_RATE_LAMBDA = 50  # passengers per minute (lambda)
+
+    NUM_SERVERS = numServers  # ID/boarding-pass queue servers
+    SERVICE_TIME = 0.75  # minutes per passenger at ID/boarding pass queue
+    averageServeTime = []
+
+    NUM_CHECKERS = 1  # personal-check queue checkers
+    MIN_SCAN_TIME = 0.5
+    MAX_SCAN_TIME = 1.0
+    averageCheckTime = []
+
+    SIM_RUN_TIME = 3000  # minutes to run the simulation
+
+    averageTotalWaitTime = []
+    averageSystemTime = []
+
+
+    class Airport(object):
+        def __init__(self, env):
+            self.env = env
+            self.serverQueue = simpy.Resource(env, capacity=NUM_SERVERS)
+            self.checkQueue = simpy.Resource(env, capacity=NUM_CHECKERS)
+
+        def serve(self):  # boarding-pass/id check
+            yield self.env.timeout(SERVICE_TIME)
+
+        def check(self):  # personal check
+            checkerTime = random.uniform(MIN_SCAN_TIME, MAX_SCAN_TIME)
+            yield self.env.timeout(checkerTime)
+
+
+    def passenger(env, name, airport):
+        # passenger arrives
+        enterSystemTime = env.now
+
+        # passenger enters ID/boarding-pass queue
+        with airport.serverQueue.request() as requestForServer:
+            startWaitingForServer = env.now
+            yield requestForServer
+            waitTimeForServer = env.now - startWaitingForServer
+            averageServeTime.append(waitTimeForServer)
+
+            yield env.process(airport.serve())
+
+        # passenger enters personal-check queue
+        with airport.checkQueue.request() as requestForChecker:
+            startWaitingForChecker = env.now
+            yield requestForChecker
+            waitTimeForChecker = env.now - startWaitingForChecker
+            averageCheckTime.append(waitTimeForChecker)
+
+            yield env.process(airport.check())
+
+        averageTotalWaitTime.append(waitTimeForServer + waitTimeForChecker)
+        averageSystemTime.append(env.now - enterSystemTime)
+
+
+    def startSimulation(env):
+        airport = Airport(env)
+
+        # Create initial passengers at time 0.0
+        for i in range(ARRIVAL_RATE_LAMBDA):
+            env.process(passenger(env, 'Passenger %d' % i, airport))
+
+        # Create more passengers while the simulation is running
+        arrivalNumber = ARRIVAL_RATE_LAMBDA - 1
+        while True:
+            # time between successive arrivals (exponential distribution)
+            yield env.timeout(np.random.exponential(ARRIVAL_RATE_LAMBDA))
+
+            # arriving passengers
+            for i in range(ARRIVAL_RATE_LAMBDA):
+                arrivalNumber += 1
+                env.process(passenger(env, 'Passenger %d' % arrivalNumber, airport))
+
+
+    # Setup and start the simulation
+    random.seed(7)
+    env = simpy.Environment()
+    env.process(startSimulation(env))
+
+    env.run(until=SIM_RUN_TIME)
+
+    print("\n")
+    print('Number of Servers in id/boarding-pass queue:', NUM_SERVERS)
+    print('Avg wait in serve queue:', np.mean(averageServeTime))
+    print('Avg wait in check queue:', np.mean(averageCheckTime))
+    print('Avg total wait time:', np.mean(averageTotalWaitTime))
+    print('Avg time in system:', np.mean(averageSystemTime))
+
+    GRAND_TOTAL_WAIT_TIME.append(np.mean(averageTotalWaitTime))
+    GRAND_TOTAL_CHECKER_TIME.append(np.mean(averageCheckTime))
+    GRAND_TOTAL_SERVER_TIME.append(np.mean(averageServeTime))
+    GRAND_TOTAL_SYSTEM_TIME.append(np.mean(averageSystemTime))
+
+
+plt.plot(range(1, 5), GRAND_TOTAL_SERVER_TIME)
+plt.plot(range(1, 5), GRAND_TOTAL_CHECKER_TIME)
+plt.plot(range(1, 5), GRAND_TOTAL_WAIT_TIME)
+plt.plot(range(1, 5), GRAND_TOTAL_SYSTEM_TIME)
+plt.xticks([1, 2, 3, 4])
+plt.xlabel("Number of Servers")
+plt.ylabel("Total Wait Time")
+plt.title("Effects of Increasing Servers on Wait Times")
+plt.legend(['Server Wait',
+            'Checker Wait',
+            'Total Wait',
+            'Time in System'],
+           loc='upper right')
+
+plt.show()
+
--- SingleTrial.py
+++ SingleTrial.py
@@ -2,9 +2,9 @@ import random
 import simpy
 import numpy as np
 
-ARRIVAL_RATE_LAMBDA = 5  # passengers per minute (lambda)
+ARRIVAL_RATE_LAMBDA = 50  # passengers per minute (lambda)
 
-NUM_SERVERS = 2  # ID/boarding-pass queue servers
+NUM_SERVERS = 3  # ID/boarding-pass queue servers
 SERVICE_TIME = 0.75  # minutes per passenger at ID/boarding pass queue
 averageServeTime = []
 
@@ -13,78 +13,66 @@ MIN_SCAN_TIME = 0.5
 MAX_SCAN_TIME = 1.0
 averageCheckTime = []
 
-SIM_RUN_TIME = 1440  # minutes to run the simulation
-REPEATS = 100
+SIM_RUN_TIME = 3000  # minutes to run the simulation
 
 averageTotalWaitTime = []
 averageSystemTime = []
 
 
-class ServerQueue(object):
-    def __init__(self, env, numServers):
+class Airport(object):
+    def __init__(self, env):
         self.env = env
-        self.machine = simpy.Resource(env, numServers)
+        self.serverQueue = simpy.Resource(env, capacity=NUM_SERVERS)
+        self.checkQueue = simpy.Resource(env, capacity=NUM_CHECKERS)
 
-    def serve(self):
+    def serve(self):  # boarding-pass/id check
         yield self.env.timeout(SERVICE_TIME)
 
-
-class CheckerQueue(object):
-    def __init__(self, env, numCheckers):
-        self.env = env
-        self.machine = simpy.Resource(env, numCheckers)
-
-    def check(self):
+    def check(self):  # personal check
         checkerTime = random.uniform(MIN_SCAN_TIME, MAX_SCAN_TIME)
         yield self.env.timeout(checkerTime)
 
 
-def passengerInBoardingPassIdQueue(env, name, serverQueue):
+def passenger(env, name, airport):
     # passenger arrives
     enterSystemTime = env.now
     print('%s arrives at the airport at %.2f.' % (name, enterSystemTime))
 
     # passenger enters ID/boarding-pass queue
-    with serverQueue.machine.request() as requestForServer:
+    with airport.serverQueue.request() as requestForServer:
         startWaitingForServer = env.now
         yield requestForServer
         waitTimeForServer = env.now - startWaitingForServer
         averageServeTime.append(waitTimeForServer)
 
         print('%s enters the ID/boarding pass queue at %.2f.' % (name, env.now))
-        yield env.process(serverQueue.serve())
+        yield env.process(airport.serve())
         print('%s leaves the ID/boarding pass queue at %.2f.' % (name, env.now))
 
-    return waitTimeForServer
-
-
-def passengerInPersonalCheckQueue(env, name, checkerQueue):
-    # passenger enter personal-check queue
-    with checkerQueue.machine.request() as requestForChecker:
+    # passenger enters personal-check queue
+    with airport.checkQueue.request() as requestForChecker:
         startWaitingForChecker = env.now
         yield requestForChecker
         waitTimeForChecker = env.now - startWaitingForChecker
         averageCheckTime.append(waitTimeForChecker)
 
         print('%s enters the personal-check queue at %.2f.' % (name, env.now))
-        yield env.process(checkerQueue.check())
+        yield env.process(airport.check())
         print('%s leaves the personal-check queue at %.2f.' % (name, env.now))
 
-    return waitTimeForChecker
+    averageTotalWaitTime.append(waitTimeForServer + waitTimeForChecker)
+    averageSystemTime.append(env.now - enterSystemTime)
 
 
-def startSimulation(env, numServers, numCheckers):
-    # Create the queues
-    serverQueue = ServerQueue(env, numServers)
-    checkerQueue = CheckerQueue(env, numCheckers)
+def startSimulation(env):
+    airport = Airport(env)
 
-    # Create 5 initial passengers at time 0
-    for i in range(5):
-        env.process(passengerInBoardingPassIdQueue(env, 'Passenger %d' % i, serverQueue))
-        env.process(passengerInPersonalCheckQueue(env, 'Passenger %d' % i, checkerQueue))
+    # Create initial passengers at time 0.0
+    for i in range(ARRIVAL_RATE_LAMBDA):
+        env.process(passenger(env, 'Passenger %d' % i, airport))
 
     # Create more passengers while the simulation is running
-    arrivalNumber = 0
+    arrivalNumber = ARRIVAL_RATE_LAMBDA - 1
     while True:
         # time between successive arrivals (exponential distribution)
         yield env.timeout(np.random.exponential(ARRIVAL_RATE_LAMBDA))
@@ -92,21 +80,18 @@ def startSimulation(env, numServers, numCheckers):
         # arriving passengers
         for i in range(ARRIVAL_RATE_LAMBDA):
             arrivalNumber += 1
-            env.process(passengerInBoardingPassIdQueue(env, 'Passenger %d' % arrivalNumber, serverQueue))
-            env.process(passengerInPersonalCheckQueue(env, 'Passenger %d' % arrivalNumber, checkerQueue))
+            env.process(passenger(env, 'Passenger %d' % arrivalNumber, airport))
 
 
 # Setup and start the simulation
 random.seed(7)
-
 env = simpy.Environment()
-env.process(startSimulation(env, NUM_SERVERS, NUM_CHECKERS))
+env.process(startSimulation(env))
 
-# Execute!
 env.run(until=SIM_RUN_TIME)
 
 print("\n")
 print('avg wait in serve queue', np.mean(averageServeTime))
 print('avg wait in check queue', np.mean(averageCheckTime))
-# print('avg total wait time', np.mean(averageTotalWaitTime))
-# print('avg time in system', np.mean(averageSystemTime))
+print('avg total wait time', np.mean(averageTotalWaitTime))
+print('avg time in system', np.mean(averageSystemTime))
